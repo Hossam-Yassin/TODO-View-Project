a9fc4e12d4d5ff2af68bc3c11f0614fabe39bf85
Hossam-Yassin <Eng.HossamYassen@gmail.com>
2020-05-14 05:01:16 +0200

    TODO View Dev : Adding Carbon components into the project

--- todo_view/src/App.js
+++ todo_view/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
+import CButton from 'carbon-components-react/lib/components/Button';
+import CTextInput from 'carbon-components-react/lib/components/TextInput';
+import CForm from 'carbon-components-react/lib/components/Form';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App  extends React.Component {
+  render() {
+    return (
+      
+      <div class="_2BSER search-header__form-wrapper">
+        <div align='center' >
+          <h1 > TODO Application </h1>
+        </div>
+        <CForm method="post" action="http://localhost:81/todoapp/api/todo">
+          <div align='center' >
+            <CTextInput  id="todoID" maxLength="40" placeholder="TODO Description"/>
+            <CButton > Add TODO </CButton>
+          </div>
+        </CForm>
+     </div>      
+    );
+  }
 }
 
 export default App;

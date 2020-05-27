58ddcd0648ee9a43d7b50d0895ed6dc95c9caf80
Hossam-Yassin <Eng.HossamYassen@gmail.com>
2020-05-27 23:40:17 +0200

    View Dev : integrate both Update and Delete TODO
    integrate both delete and update todo status with the backend

--- todo_view/src/App.js
+++ todo_view/src/App.js
@@ -10,11 +10,10 @@ import { ThumbsUp16 , TrashCan16} from '@carbon/icons-react';
 import '../node_modules/carbon-components/css/carbon-components.css';
 import './App.css';
 
-
 class App  extends React.Component {
   render() {
     return (
-   <CForm class="App" action="http://localhost:81/todoapp/api/todo" method="post"  id="todo_form">
+   <CForm class="App" action="http://localhost:81/todoapp/api/todo/" method="post"  id="todo_form">
       <br/>
       <legend class="h4"> TODO APPLICATION </legend>
       <br/>
@@ -30,22 +29,46 @@ class App  extends React.Component {
       <br/><br/><br/><br/>
       <TODO_LIST_COMP/>
    </CForm>
-  
     );
   }
 }
 
 class TODO_LIST_COMP extends React.Component{
-  
   constructor(){
     super();
     this.state = {
         list_todos: []
     }
-  } 
+  }
+
+  deleteTODO(id) {
+    let url = 'http://localhost:81/todoapp/api/todo/'+id+'/';
+    fetch(url, {
+            method: 'DELETE',
+            headers: {
+              'x-gateway-apikey': 'ConfiguredValue',
+              'csrf-token': 'CrossSiteRequestForgery_Token'
+            }
+          });
+  };
+
+  completeTODO(id) {
+    let url = 'http://localhost:81/todoapp/api/todo/'+id+'/';
+    fetch(url, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+              'x-gateway-apikey': 'ConfiguredValue',
+              'csrf-token': 'CrossSiteRequestForgery_Token'
+            },
+            body: JSON.stringify({
+              status:'Completed'
+            })
+          });
+  };
 
   componentDidMount() {
-    let url = 'http://localhost:81/todoapp/api/todos';
+    let url = 'http://localhost:81/todoapp/api/todos/';
 
     fetch(url , {
       method: 'GET',
@@ -68,18 +91,21 @@ class TODO_LIST_COMP extends React.Component{
           {
           this.state.list_todos.map(
             (todo, index) =>  
-            <CListItem> 
-              <div class="column">{todo.description}</div>
-              <div class="column"><ThumbsUp16 /> &emsp; <TrashCan16 /> </div>
+            <CListItem  > 
+              <div style={{ textDecoration: todo.status=='Completed' ? "line-through" : "" }} class="column">{todo.description}</div>
+              <div class="column">
+                <ThumbsUp16 onClick={() => this.completeTODO(todo.id)} /> &emsp; 
+                <TrashCan16 onClick={() => this.deleteTODO(todo.id)}/> 
+              </div>
               <br/>
             </CListItem>
             )
             
           }
         </COrderedList> 
-      </div>
+        </div>
+
       );
    }
 }
-
 export default App;

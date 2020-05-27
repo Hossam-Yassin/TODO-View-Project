4f98d8cbf287aa217eb2d71a801f92178b6f3fbe
Hossam-Yassin <Eng.HossamYassen@gmail.com>
2020-05-27 22:29:54 +0200

    View Dev : integrate witht the TODO List Endpoint
    Add skelton for the view list with integration with the todos_list end point

--- todo_view/src/App.js
+++ todo_view/src/App.js
@@ -5,8 +5,9 @@ import CTextInput from 'carbon-components-react/lib/components/TextInput';
 import CForm from 'carbon-components-react/lib/components/Form';
 import COrderedList from 'carbon-components-react/lib/components/OrderedList';
 import CListItem from 'carbon-components-react/lib/components/ListItem';
+import { ThumbsUp16 , TrashCan16} from '@carbon/icons-react';
 
-import '../node_modules/carbon-components/css/carbon-components.css'
+import '../node_modules/carbon-components/css/carbon-components.css';
 import './App.css';
 
 
@@ -27,10 +28,58 @@ class App  extends React.Component {
       </div> 
 
       <br/><br/><br/><br/>
-      <CListItem style= {{ textDecoration: 'line-through'} }> Test Completed</CListItem>
+      <TODO_LIST_COMP/>
    </CForm>
   
     );
   }
 }
+
+class TODO_LIST_COMP extends React.Component{
+  
+  constructor(){
+    super();
+    this.state = {
+        list_todos: []
+    }
+  } 
+
+  componentDidMount() {
+    let url = 'http://localhost:81/todoapp/api/todos';
+
+    fetch(url , {
+      method: 'GET',
+      headers: {
+        'x-gateway-apikey': 'ConfiguredValue',
+        'csrf-token': 'CrossSiteRequestForgery_Token'
+      }
+    })
+    .then(response => response.json())
+    .then((data)=>{
+      this.setState({list_todos: data.todos})
+    })
+  }
+  
+   render() {
+      return (
+
+        <div class="row App" >
+        <COrderedList>
+          {
+          this.state.list_todos.map(
+            (todo, index) =>  
+            <CListItem> 
+              <div class="column">{todo.description}</div>
+              <div class="column"><ThumbsUp16 /> &emsp; <TrashCan16 /> </div>
+              <br/>
+            </CListItem>
+            )
+            
+          }
+        </COrderedList> 
+      </div>
+      );
+   }
+}
+
 export default App;
